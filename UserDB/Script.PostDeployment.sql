eb02039485d9bbc8f6e045b4746b13ebdbc7f2b1
Gregory Fox <gregory.thomas.fox@gmail.com>
2023-03-13 18:16:26 +0000

    Addred post deployment script

---
+++ UserDB/Script.PostDeployment.sql
@@ -0,0 +1,16 @@
+﻿/*
+Post-Deployment Script						
+--------------------------------------------------------------------------------------
+Populates the users DB with Dummy Data if none already exists			
+--------------------------------------------------------------------------------------
+*/
+
+if not exists (select 1 from dbo.[user])
+begin
+	insert into dbo.[user] (FirstName, LastName)
+	values ('Greg', 'Fox'),
+	('Lucy', 'Ellis'),
+	('Samwise', 'Ceaser'),
+	('John', 'Smith'),
+	('Mary', 'Jones');
+end 
